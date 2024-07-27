1fa4d42e29d1efe97a47426324d762c977a0a519
Gevorg Melkumyan <gevorgmelkoumyan@gmail.com>
2024-07-27 22:18:59 +0400

    Added a socket docker service

---
+++ app/Events/TestEVent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+
+class TestEVent implements ShouldBroadcastNow {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public string $message) {}
+
+    public function broadcastOn() {
+        Redis::set('asdasd', 'dsfsdfdsf');
+        Log::debug('Broadcasting message: ' . Redis::get('asdasd'));
+        return new Channel('messages');
+    }
+
+    public function broadcastAs(): string {
+        return 'message';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Events\TestEVent;
 use App\Jobs\ImportCSVJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    ImportCSVJob::dispatch('customers_2.csv');
-
+//    ImportCSVJob::dispatch('customers_2.csv');
+    broadcast(new TestEVent('Hello World!'));
     return 'Job dispatched!';
 });
